e85f8ff65e13329e8e36ba6e0fb2105d82014dd6
凤之兮原 <1577310596@qq.com>
2023-09-25 20:12:41 +0800

    feat: add customized module

---
+++ packages/customized/canvas.ts
@@ -0,0 +1,6 @@
+// TODO: 这里可以自由定制画布大小
+export const CanvasConfig = {
+	width: 600,
+	height: 800,
+	background: '#fff'
+}
---
+++ packages/customized/color.ts
@@ -0,0 +1,10 @@
+// TODO:这里使用一个变量,可以方便后期自由定制
+export const ColorTheme = {
+	black: '#001529',
+}
+
+export const LayoutColor = {
+	leftSiderBackground: 'white',
+	rightSiderBackground: 'white',
+
+}
