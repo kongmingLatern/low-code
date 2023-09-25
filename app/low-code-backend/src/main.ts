ec5d5b18bcaa3f7bc318ef12bad4fd68dcc9036d
凤之兮原 <1577310596@qq.com>
2023-09-25 18:58:06 +0800

    refactor: refactor server module

--- app/low-code-backend/src/main.ts
+++ app/low-code-backend/src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GatewayModule } from './module/gateway/gateway.module';
 
-async function bootstrap() {
+async function createWebSocketServer() {
   const app = await NestFactory.create(GatewayModule);
-  const http = await NestFactory.create(AppModule);
   await app.listen(3000);
+}
+
+async function createHttpServer() {
+  const http = await NestFactory.create(AppModule);
   await http.listen(3333);
 }
-bootstrap();
+
+createWebSocketServer();
+createHttpServer();
