42ee92af8dfa399d674c696930c32a2bca28a7fd
凤之兮原 <1577310596@qq.com>
2023-09-25 09:57:24 +0800

    feat: add socket-io event

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import { get } from '@/api'
+import { get, post } from '@/api'
+import { io } from 'socket.io-client'
 
+const url = 'http://localhost:3000'
+let socket
+function createSocket() {
+	const socket = io(url)
+	return socket
+}
+
+function connect() {
+	socket = createSocket()
+
+	socket.on('connect', () => {
+		console.log('open')
+	})
+
+	socket.on('join', data => {
+		console.log('join', data)
+	})
+}
+
+function sendMessage() {
+	socket.emit(
+		'onJoin',
+		{
+			value: '一串连接信息',
+		},
+		(e: any) => {
+			console.log(e)
+		}
+	)
+}
 function App() {
 	return (
 		<>
-			<button onClick={() => get('/')}>123</button>
+			<button onClick={() => connect()}>连接</button>
+
+			<button onClick={() => sendMessage()}>
+				发送信息
+			</button>
+			<button onClick={() => get('/', { a: 123 })}>
+				123
+			</button>
+			<button onClick={() => post('/', { a: 1 })}>
+				123
+			</button>
 		</>
 	)
 }
