05563e05b1f1735ee47acb7c821889501522addc
凤之兮原 <1577310596@qq.com>
2023-09-26 13:47:55 +0800

    chore: finish basic drop event

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,10 +1,24 @@
 import classNames from 'classnames'
 import styled from './box.module.scss'
 import { Adapter } from '@packages/renderer-core'
-export default function Box(props) {
-	const { type, value, ...rest } = props
+import { useRef, useState } from 'react'
+import { useDrag } from 'ahooks'
 
+export default function Box(props) {
+	const { type, value, data, ...rest } = props
 	const adapter = new Adapter(type, value, rest)
+
+	const dragRef = useRef(null)
+	const [dragging, setDragging] = useState(false)
+
+	useDrag(data, dragRef, {
+		onDragStart: () => {
+			setDragging(true)
+		},
+		onDragEnd: () => {
+			setDragging(false)
+		},
+	})
 
 	function addToCanvas(props) {
 		console.log('props', props)
@@ -17,6 +31,7 @@ export default function Box(props) {
 				'select-none',
 				styled.border
 			)}
+			ref={dragRef}
 			onClick={() => addToCanvas(props)}
 		>
 			<span className="text-20px font-semibold h-80px lh-80px">
--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,11 +1,34 @@
 import { CanvasConfig } from '@packages/customized'
 import classNames from 'classnames'
 import styled from './canvas.module.scss'
+import { useRef } from 'react'
+import { useDrop } from 'ahooks'
 
 export default function Canvas() {
+	const dropRef = useRef(null)
+
+	useDrop(dropRef, {
+		onText: (text, e) => {
+			console.log(e)
+			alert(`'text: ${text}' dropped`)
+		},
+		onFiles: (files, e) => {
+			console.log(e, files)
+			alert(`${files.length} file dropped`)
+		},
+		onUri: (uri, e) => {
+			console.log(e)
+			alert(`uri: ${uri} dropped`)
+		},
+		onDom: (content: string, e) => {
+			alert(`custom: ${content} dropped, ${e}`)
+		},
+	})
+
 	return (
 		<div
 			id="canvas"
+			ref={dropRef}
 			className={classNames(styled.coverCanvas)}
 			style={{
 				backgroundColor: CanvasConfig.background,
