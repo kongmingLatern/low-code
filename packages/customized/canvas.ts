7cb0f9b2e4ae7a65577fa70c5da742a41995fe18
凤之兮原 <1577310596@qq.com>
2023-09-27 09:10:36 +0800

    feat: add updateSelectedElement

--- packages/customized/canvas.ts
+++ packages/customized/canvas.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid'
 // TODO: 这里可以自由定制画布大小
-export const CanvasConfig = {
+const CanvasConfig = {
 	element: [],
 
 	style: {
@@ -13,21 +13,49 @@ export const CanvasConfig = {
 export class Canvas {
 	canvas: Record<string, any>
 	listeners: Array<any> = []
+	selectedIndex: number | null
 
 	constructor(_canvas: Record<string, any> = CanvasConfig) {
 		this.canvas = _canvas
 		this.listeners = []
+		this.selectedIndex = null
 	}
 	addElement(element) {
+		// 添加元素
 		this.canvas.element.push({
 			...element,
 			key: uuid(),
 		})
+
+		// 设置为选中元素
+		this.selectedIndex = this.canvas.element.length - 1
+
+		// 更新画布
 		this.update()
 	}
 
 	getCanvas() {
 		return { ...this.canvas }
+	}
+
+	getSelectedIndex() {
+		return this.selectedIndex
+	}
+
+	getSelectedElement() {
+		if (this.selectedIndex) {
+			return this.canvas.element[this.selectedIndex]
+		}
+		return {}
+	}
+
+	setSelectedIndex(index) {
+		if (isEqual(this.selectedIndex, index)) {
+			return
+		}
+		this.selectedIndex = index
+
+		this.update()
 	}
 
 	setCanvas(_canvas) {
@@ -52,6 +80,21 @@ export class Canvas {
 		this.listeners.forEach(listener => listener())
 	}
 
+	updateSelectedElement(newStyle = {}, value?) {
+		const selectedElement = this.getSelectedElement()
+		Object.assign(
+			this.canvas.element[this.selectedIndex || 0],
+			{
+				style: {
+					...(selectedElement.style || {}),
+					...newStyle,
+				},
+				value,
+			}
+		)
+		this.update()
+	}
+
 	subscribe(listener) {
 		this.listeners.push(listener)
 		return () =>
@@ -64,11 +107,18 @@ export class Canvas {
 		const obj = {
 			addElement: this.addElement,
 			getCanvas: this.getCanvas,
+			getSelectedIndex: this.getSelectedIndex,
+			setSelectedIndex: this.setSelectedIndex,
 			setCanvas: this.setCanvas,
 			setStyle: this.setStyle,
 			subscribe: this.subscribe,
 			clearCanvas: this.clearCanvas,
+			updateSelectedElement: this.updateSelectedElement,
 		}
 		return obj
 	}
+}
+
+function isEqual(selectedIndex, index) {
+	return selectedIndex === index
 }
