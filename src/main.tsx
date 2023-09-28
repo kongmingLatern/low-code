d9ddecd324bdf3620b2cb66dd48681176c5d26b3
凤之兮原 <1577310596@qq.com>
2023-09-28 15:45:16 +0800

    chore: add reset css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.tsx'
 import 'uno.css'
+import './index.css'
 
 ReactDOM.createRoot(
 	document.getElementById('root')!
