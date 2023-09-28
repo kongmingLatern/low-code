a156911fbbaaa9380e5a6209397407751c5b65a9
凤之兮原 <1577310596@qq.com>
2023-09-28 09:29:35 +0800

    feat: support change size

--- src/components/Element/index.tsx
+++ src/components/Element/index.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 import { useCanvasContext } from '@/hooks'
 
 export default function Element(props) {
-	const { type, value, style } = props.element
+	const { key, type, value, style } = props.element
 
 	const { index, isSelected } = props
 	const dragRef = useRef(null)
@@ -21,7 +21,6 @@ export default function Element(props) {
 			e.dataTransfer.setData('text', startX + ',' + startY)
 		},
 	})
-
 	const renderAdapter = new RenderAdapter(
 		type,
 		value,
@@ -43,6 +42,7 @@ export default function Element(props) {
 				child.props.className,
 				isSelected && styled.selected
 			),
+			'data-key': key,
 			// style: { ...child.props.style, position: 'absolute' },
 			style: { ...child.props.style },
 		})
@@ -59,20 +59,117 @@ export default function Element(props) {
 			onClick={setSelected}
 		>
 			<ElementChildren />
-			{isSelected && (
-				<>
-					<i className="bg-blue-300 circle absolute top-0 left-0 w-8px h-8px"></i>
-					<i className="bg-blue-300 circle absolute top-0 left-[50%] w-8px h-8px translate-x-[-50%]"></i>
-					<i className="bg-blue-300 circle absolute top-0 left-[100%] w-8px h-8px translate-x-[-100%]"></i>
-
-					<i className="bg-blue-300 circle absolute top-[50%] left-0 w-8px h-8px translate-y-[-50%]"></i>
-					<i className="bg-blue-300 circle absolute top-[50%] left-[100%] w-8px h-8px translate-x-[-100%] translate-y-[-50%]"></i>
-					<i className="bg-blue-300 circle absolute top-[100%] left-0 w-8px h-8px translate-y-[-100%]"></i>
-
-					<i className="bg-blue-300 circle absolute top-[100%] left-[50%] w-8px h-8px translate-x-[-50%] translate-y-[-100%]"></i>
-					<i className="bg-blue-300 circle absolute top-[100%] left-[100%] w-8px h-8px translate-x-[-100%] translate-y-[-100%]"></i>
-				</>
-			)}
+			{isSelected && <CircleList canvas={canvas} />}
 		</div>
+	)
+}
+function CircleList(props): React.ReactNode {
+	const { canvas } = props
+
+	const handleMouseDown = e => {
+		const direction = e.target.dataset.direction
+		if (!direction) {
+			return
+		}
+		e.stopPropagation()
+		e.preventDefault()
+
+		let startX = e.pageX
+		let startY = e.pageY
+
+		const move = e => {
+			function transform(style, value) {
+				if (style === 'auto') {
+					return value
+				} else {
+					return Number(style)
+				}
+			}
+			const x = e.pageX
+			const y = e.pageY
+			let disX = x - startX
+			let disY = y - startY
+			const selectedElement = canvas.getSelectedElement()
+			const { key } = selectedElement
+			const targetDom: any = document.querySelector(
+				`[data-key="${key}"]`
+			)
+
+			const newStyle: Record<string, any> = {}
+
+			if (direction) {
+				if (direction.indexOf('top') >= 0) {
+					disY = 0 - disY
+					newStyle.top = selectedElement.style.top - disY
+				} else if (direction.indexOf('left') >= 0) {
+					disX = 0 - disX
+					newStyle.left = selectedElement.style.left - disX
+				}
+			}
+
+			Object.assign(newStyle, {
+				width:
+					transform(
+						selectedElement.style.width,
+						targetDom.offsetWidth
+					) + disX,
+				height:
+					transform(
+						selectedElement.style.height,
+						targetDom.offsetHeight
+					) + disY,
+			})
+
+			canvas.updateSelectedElement(newStyle)
+
+			startX = x
+			startY = y
+		}
+
+		const up = () => {
+			document.removeEventListener('mousemove', move)
+			document.removeEventListener('mouseup', up)
+		}
+
+		document.addEventListener('mousemove', move)
+		document.addEventListener('mouseup', up)
+	}
+	return (
+		<ul onMouseDown={handleMouseDown}>
+			<li
+				className="bg-blue-600 circle absolute top-0 left-0 w-5px h-5px"
+				data-direction="top, left"
+			></li>
+			<li
+				className="bg-blue-600 circle absolute top-0 left-[50%] w-5px h-5px translate-x-[-50%]"
+				data-direction="top, center"
+			></li>
+			<li
+				className="bg-blue-600 circle absolute top-0 left-[100%] w-5px h-5px translate-x-[-100%]"
+				data-direction="top, right"
+			></li>
+
+			<li
+				className="bg-blue-600 circle absolute top-[50%] left-0 w-5px h-5px translate-y-[-50%]"
+				data-direction="center, left"
+			></li>
+			<li
+				className="bg-blue-600 circle absolute top-[50%] left-[100%] w-5px h-5px translate-x-[-100%] translate-y-[-50%]"
+				data-direction="center, right"
+			></li>
+
+			<li
+				className="bg-blue-600 circle absolute top-[100%] left-0 w-5px h-5px translate-y-[-100%]"
+				data-direction="bottom, left"
+			></li>
+			<li
+				className="bg-blue-600 circle absolute top-[100%] left-[50%] w-5px h-5px translate-x-[-50%] translate-y-[-100%]"
+				data-direction="bottom, center"
+			></li>
+			<li
+				className="bg-blue-600 circle absolute top-[100%] left-[100%] w-5px h-5px translate-x-[-100%] translate-y-[-100%]"
+				data-direction="bottom, right"
+			></li>
+		</ul>
 	)
 }
