643b909bf77e659edb5c7003db347cb44df5246f
凤之兮原 <1577310596@qq.com>
2023-10-08 15:03:18 +0800

    chore: format

--- app/low-code-backend/src/main.ts
+++ app/low-code-backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GatewayModule } from './module/gateway/gateway.module';
-import { RedisModule } from './module/redis/redis.module';
 
 async function createWebSocketServer() {
   const app = await NestFactory.create(GatewayModule);
@@ -13,10 +12,5 @@ async function createHttpServer() {
   await http.listen(3333);
 }
 
-// async function createRedisServer() {
-//   await NestFactory.create(RedisModule);
-// }
-
 createWebSocketServer();
 createHttpServer();
-// createRedisServer();
