514b2cf3d8e6e9e6cc5c198db3ef35cbd56252bc
凤之兮原 <1577310596@qq.com>
2023-10-18 10:58:46 +0800

    chore: add row type

--- app/low-code-frontend/src/components/common/RowItem/index.tsx
+++ app/low-code-frontend/src/components/common/RowItem/index.tsx
@@ -2,23 +2,49 @@ import { Row, Col } from 'antd'
 import { Gutter } from 'antd/es/grid/row'
 import { FunctionComponent } from 'react'
 
+declare const RowAligns: readonly [
+	'top',
+	'middle',
+	'bottom',
+	'stretch'
+]
+declare const RowJustify: readonly [
+	'start',
+	'end',
+	'center',
+	'space-around',
+	'space-between',
+	'space-evenly'
+]
+
 interface RowItemProps {
 	gutter: Gutter
 	count: number
+	list: any[]
 	children: React.ReactNode
+	align?: (typeof RowAligns)[number]
+	justify?: (typeof RowJustify)[number]
 }
 
 const RowItem: FunctionComponent<
 	Partial<RowItemProps>
 > = props => {
-	const { gutter, count = 1, children } = props
+	const {
+		gutter,
+		list,
+		count = list?.length || 4,
+		justify = 'start',
+		align = 'middle',
+	} = props
 	return (
-		<Row gutter={gutter}>
-			{new Array(count).fill(0).map((_, index) => (
-				<Col key={index} span={24 / count}>
-					{children}
-				</Col>
-			))}
+		<Row gutter={gutter} justify={justify} align={align}>
+			{list?.map(i => {
+				return (
+					<Col key={i.key} span={24 / count}>
+						{i}
+					</Col>
+				)
+			})}
 		</Row>
 	)
 }
