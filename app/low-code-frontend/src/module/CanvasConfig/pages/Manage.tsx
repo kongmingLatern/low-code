7a8e7d1d15fe904fbd9e6d8c52fc98b373c283ef
凤之兮原 <1577310596@qq.com>
2023-10-27 09:07:35 +0800

    chore: add basic Manage content

--- app/low-code-frontend/src/module/CanvasConfig/pages/Manage.tsx
+++ app/low-code-frontend/src/module/CanvasConfig/pages/Manage.tsx
@@ -1,95 +1,61 @@
-import DataTable from '@/components/common/DataTable'
-import { formatYMD } from '@/shared'
-import { Button, Space, Tag } from 'antd'
-import { ColumnsType } from 'antd/es/table'
-interface DataType {
-	key: string
-	canvas_name: string
-	num: number
-	create_time: string
-	tags: string[]
-}
+import React from 'react'
+import { Badge, Descriptions } from 'antd'
+import type { DescriptionsProps } from 'antd'
 
-export default function Manage() {
-	const columns: ColumnsType<DataType> = [
-		{
-			title: '画布名称',
-			dataIndex: 'canvas_name',
-			key: 'canvas_name',
-			align: 'center',
-		},
-		{
-			title: '创建时间',
-			dataIndex: 'create_time',
-			key: 'create_time',
-			align: 'center',
-		},
-		{
-			title: '协作人数',
-			dataIndex: 'num',
-			key: 'num',
-			align: 'center',
-		},
+const items: DescriptionsProps['items'] = [
+	{
+		key: '1',
+		label: '项目名称',
+		children: 'XXX 管理系统',
+	},
+	{
+		key: 'person',
+		label: '项目管理员',
+		children: 'ckh',
+		span: 3,
+	},
+	{
+		key: '7',
+		label: '项目人员数',
+		children: 13,
+	},
+	{
+		key: '8',
+		label: '画布数量',
+		children: 13,
+	},
+	{
+		key: '4',
+		label: '创建时间',
+		children: '2023-10-24 18:00:00',
+		span: 3,
+	},
+	{
+		key: 'update_time',
+		label: '最新更新时间',
+		children: '2023-10-25 18:30:00',
+		span: 3,
+	},
+	{
+		key: '6',
+		label: '项目状态',
+		children: <Badge status="processing" text="进行中" />,
+		span: 3,
+	},
+	{
+		key: '2',
+		label: '项目邀请码',
+		children: '513123',
+	},
+]
 
-		{
-			title: '画布状态',
-			key: 'tags',
-			dataIndex: 'tags',
-			align: 'center',
-			render: (_, { tags }) => (
-				<>
-					{tags.map(tag => {
-						const color =
-							tag === '完成'
-								? 'geekblue'
-								: tag === '进行中'
-								? 'green'
-								: 'red'
-						return (
-							<Tag color={color} key={tag}>
-								{tag.toUpperCase()}
-							</Tag>
-						)
-					})}
-				</>
-			),
-		},
-		{
-			title: '操作',
-			key: 'action',
-			align: 'center',
-			render: () => (
-				<Space size="middle">
-					<Button type="link">进入画布</Button>
-					<Button type="primary">修改</Button>
-					<Button danger>删除</Button>
-				</Space>
-			),
-		},
-	]
+const App: React.FC = () => (
+	<Descriptions
+		className="p-1.5rem"
+		title="项目信息"
+		bordered
+		items={items}
+	/>
+)
 
-	const data: DataType[] = [
-		{
-			key: '1',
-			canvas_name: 'John Brown',
-			num: 1,
-			create_time: formatYMD(new Date()),
-			tags: ['已完成'],
-		},
-		{
-			key: '2',
-			canvas_name: 'Jim Green',
-			num: 2,
-			create_time: formatYMD(new Date()),
-			tags: ['进行中'],
-		},
-		{
-			key: '3',
-			canvas_name: 'Joe Black',
-			num: 23,
-			create_time: formatYMD(new Date()),
-			tags: ['未开始'],
-		},
-	]
-	return <DataTable columns={columns} dataSource={data} />
-}
+export default App
