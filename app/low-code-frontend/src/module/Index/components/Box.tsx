8ef64833e6e11bf39a6f2a78632ab2f97868b54d
凤之兮原 <1577310596@qq.com>
2023-11-08 21:15:11 +0800

    feat: enhance Box component

--- app/low-code-frontend/src/module/Index/components/Box.tsx
+++ app/low-code-frontend/src/module/Index/components/Box.tsx
@@ -7,13 +7,24 @@ interface BoxProps {
 		src?: string
 		width?: number | string
 		height?: number | string
+		color?: string
 	}
 	content?: any
 	fontSize?: string | number
+	color?: string
 }
 
-const Box: FunctionComponent<BoxProps> = props => {
-	const { showIcon = true, icon, content, fontSize } = props
+const Box: FunctionComponent<
+	BoxProps & Record<string, any>
+> = props => {
+	const {
+		showIcon = true,
+		icon,
+		content,
+		fontSize,
+		color = 'black',
+		...style
+	} = props
 	return (
 		<span className="flex-center">
 			{showIcon && (
@@ -21,9 +32,16 @@ const Box: FunctionComponent<BoxProps> = props => {
 					icon={icon?.src || 'fad:logo-fl'}
 					width={icon?.width || 60}
 					height={icon?.width || 60}
+					color={icon?.color || 'black'}
 				/>
 			)}
-			<span style={{ fontSize: fontSize || '16px' }}>
+			<span
+				style={{
+					fontSize: fontSize || '16px',
+					color,
+					...style,
+				}}
+			>
 				{content ? content : '用户名'}
 			</span>
 		</span>
