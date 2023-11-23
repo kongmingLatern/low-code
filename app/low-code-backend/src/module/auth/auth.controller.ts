1e448c6e721126cf20c52db959339b518a08c66f
凤之兮原 <1577310596@qq.com>
2023-11-23 22:26:19 +0800

    feat: add basic login and register handler

--- app/low-code-backend/src/module/auth/auth.controller.ts
+++ app/low-code-backend/src/module/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
-@Controller('')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async signIn(@Body() user: LoginDto) {
+    return await this.authService.signIn(user);
+  }
+
+  @Post('register')
+  async signUp(@Body() user: RegisterDto) {
+    return await this.authService.signUp(user);
+  }
 }
--- app/low-code-backend/src/module/auth/auth.service.ts
+++ app/low-code-backend/src/module/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signIn(user: LoginDto) {
+    console.log('login');
+    return user;
+  }
+
+  signUp(user: RegisterDto) {
+    console.log('register');
+    return user;
+  }
+}
