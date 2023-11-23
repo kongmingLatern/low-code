43cf9b9f719edbe39819711025dbad9e1e29b571
凤之兮原 <1577310596@qq.com>
2023-11-23 22:53:46 +0800

    feat: finish basic login and register api function

--- app/low-code-backend/src/module/auth/auth.service.ts
+++ app/low-code-backend/src/module/auth/auth.service.ts
@@ -1,16 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { User } from '../user/entities/user.entity';
+import * as md5 from 'md5';
 
 @Injectable()
 export class AuthService {
-  signIn(user: LoginDto) {
-    console.log('login');
-    return user;
+  @InjectEntityManager()
+  private userRepository: EntityManager;
+  private logger = new Logger();
+
+  async signIn(user: LoginDto) {
+    const { username, password } = user;
+    const foundUser = await this.userRepository.findOneBy(User, {
+      username,
+      password: md5(password),
+    });
+    if (!foundUser) {
+      throw new HttpException('登陆失败', HttpStatus.OK);
+    } else {
+      return '登陆成功';
+    }
   }
 
-  signUp(user: RegisterDto) {
-    console.log('register');
-    return user;
+  async signUp(user: RegisterDto) {
+    const { username, password } = user;
+    const foundUser = await this.userRepository.findOneBy(User, { username });
+    if (foundUser) {
+      throw new HttpException('该用户名已经存在,请重新输入', HttpStatus.OK);
+    }
+    const newUser = new User();
+    newUser.username = username;
+    newUser.password = md5(password);
+    try {
+      await this.userRepository.save(newUser);
+      return '注册成功';
+    } catch (e) {
+      this.logger.error(e, AuthService);
+      throw new HttpException('注册失败', HttpStatus.BAD_REQUEST);
+    }
   }
 }
