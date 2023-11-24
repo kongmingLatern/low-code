b39e956aa46c65c4bb36622c03297d16719b06aa
凤之兮原 <1577310596@qq.com>
2023-11-24 10:03:07 +0800

    feat: add LoginGuard

---
+++ app/low-code-backend/src/guard/login.guard.ts
@@ -0,0 +1,41 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+
+    const authorization = request.header('authorization') || '';
+
+    const bearer = authorization.split(' ');
+
+    if (!bearer || bearer.length < 2) {
+      throw new UnauthorizedException('登录 token 错误');
+    }
+
+    const token = bearer[1];
+
+    try {
+      const info = this.jwtService.verify(token);
+      (request as any).user = info.user;
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException('登录 token 失效，请重新登录');
+    }
+  }
+}
