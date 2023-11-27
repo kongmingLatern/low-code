afaf667c828dcd873ecd09a5578b98d90dcd0aa4
凤之兮原 <1577310596@qq.com>
2023-11-27 20:44:07 +0800

    chore: format

--- app/low-code-backend/src/module/gateway/gateway.gateway.ts
+++ app/low-code-backend/src/module/gateway/gateway.gateway.ts
@@ -26,7 +26,6 @@ export class GatewayGateway {
     @MessageBody() body: any,
     @ConnectedSocket() client: Socket,
   ) {
-    // TODO: 获取画布id
     const { canvasId } = body;
     const { data } = await this.redisService.getCanvas(canvasId);
     client.join(`canvas_${canvasId}`);
@@ -47,10 +46,10 @@ export class GatewayGateway {
     @MessageBody() body: any,
     @ConnectedSocket() client: Socket,
   ) {
-    // NOTE: 这里会广播给其他的客户端
+    // NOTE: 这里会广播给其他进入当前画布的客户端
     client.to(`canvas_${body.canvasId}`).emit('active', body);
-    // client.broadcast.emit('active', body);
   }
+
   @SubscribeMessage('onCanvasUpdate')
   async handleCanvasUpdate(
     @MessageBody() body: any,
@@ -63,7 +62,7 @@ export class GatewayGateway {
       element: data.element,
       style: data.style,
     });
-    // client.broadcast.emit('canvasUpdate', body);
+    // 更新当前画布信息至所有进入该画布的用户
     client.to(`canvas_${data.canvasId}`).emit('canvasUpdate', body);
   }
 }
