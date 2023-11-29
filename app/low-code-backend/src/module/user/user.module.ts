aefb314e3dcdf32953fb3f09d9a595f38b380930
凤之兮原 <1577310596@qq.com>
2023-11-29 10:13:45 +0800

    chore: use forwardRef to avoid circluar reference

--- app/low-code-backend/src/module/user/user.module.ts
+++ app/low-code-backend/src/module/user/user.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ProjectService } from '../project/project.service';
 import { RoleService } from '../role/role.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
+import { UserProjectRoleService } from 'src/joinTable/user_project_role/user_project_role.service';
 import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, RoleService],
+  providers: [UserService, ProjectService, RoleService, UserProjectRoleService],
   exports: [UserModule],
 })
 export class UserModule {}
--- app/low-code-backend/src/module/user/user.service.ts
+++ app/low-code-backend/src/module/user/user.service.ts
@@ -1,16 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  forwardRef,
+} from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { EntityManager } from 'typeorm';
 import { InjectEntityManager } from '@nestjs/typeorm';
+import { ProjectService } from '../project/project.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { UserProjectRoleService } from 'src/joinTable/user_project_role/user_project_role.service';
 import { v4 } from 'uuid';
 
 @Injectable()
 export class UserService {
   @InjectEntityManager()
   private userRepository: EntityManager;
+
+  @Inject(forwardRef(() => ProjectService))
+  private projectService: ProjectService;
+
+  @Inject()
+  private userProjectRoleService: UserProjectRoleService;
 
   async createUser(createUserDto: CreateUserDto) {
     const user = {
@@ -39,6 +53,24 @@ export class UserService {
   }
 
   async delete(uid: string) {
-    return await this.userRepository.delete(User, uid);
+    try {
+      const { project } = await this.projectService.findAllByUid(uid);
+
+      await Promise.all(
+        project.map(async (p) => {
+          const { project_id, createBy } = p;
+          // 如果 createBy === uid,那么表示该项目是由该用户自己创建的,要去删掉对应的项目
+          if (createBy === uid) {
+            await this.projectService.remove(project_id);
+          }
+          // 连接表中的信息都会进行删除
+          await this.userProjectRoleService.deleteById({ uid, project_id });
+        }),
+      );
+      await this.userRepository.delete(User, uid);
+    } catch (e) {
+      throw new HttpException('删除失败,原因是:', e);
+    }
+    return '删除成功';
   }
 }
