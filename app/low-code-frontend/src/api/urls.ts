004de59a19772db3c587f39c9b19ce785cd937e8
凤之兮原 <1577310596@qq.com>
2023-12-04 11:51:06 +0800

    chore: add urls about project

--- app/low-code-frontend/src/api/urls.ts
+++ app/low-code-frontend/src/api/urls.ts
@@ -1,9 +1,13 @@
 const modules = {
 	auth: '/auth',
+	project: '/project',
 }
 export const urls = {
 	auth: {
 		login: `${modules.auth}/login`,
 		register: `${modules.auth}/register`,
+	},
+	project: {
+		getAllProjectByUid: `${modules.project}/getAllProject`,
 	},
 }
