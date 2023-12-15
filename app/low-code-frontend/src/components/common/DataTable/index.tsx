1c7048be457c2c42949fc9c505f560d9f45c0d1b
凤之兮原 <1577310596@qq.com>
2023-12-15 09:58:29 +0800

    feat: enhance dataTable

--- app/low-code-frontend/src/components/common/DataTable/index.tsx
+++ app/low-code-frontend/src/components/common/DataTable/index.tsx
@@ -1,9 +1,11 @@
 import { Table, TableProps } from 'antd'
 
+import { ColumnType } from 'antd/es/table'
 import React from 'react'
 
 export interface TableEnhanceProps extends TableProps<any> {
 	primaryKey?: string
+	showIndex?: boolean
 }
 
 const App: React.FC<TableEnhanceProps> = props => {
@@ -11,8 +13,27 @@ const App: React.FC<TableEnhanceProps> = props => {
 		primaryKey = '',
 		columns,
 		dataSource,
+		showIndex = true,
 		...rest
 	} = props
+
+	if (showIndex) {
+		const index: ColumnType<any> = {
+			title: '序号',
+			dataIndex: 'index',
+			key: 'index',
+			align: 'center',
+			render: (_, __, i) => <span>{i + 1}</span>,
+			width: 60,
+			ellipsis: true
+		}
+
+		if (columns?.every(i => i.key !== index.key)) {
+			columns?.unshift(index)
+		}
+	}
+
+
 
 	const items = columns?.map(i => {
 		return {
@@ -32,6 +53,7 @@ const App: React.FC<TableEnhanceProps> = props => {
 					}
 				})
 			}
+			scroll={{ x: 1300 }}
 			{...rest}
 		/>
 	)
