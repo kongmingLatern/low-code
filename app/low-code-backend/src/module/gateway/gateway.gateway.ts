b108c501622573372933e3104448a9c72acc6d66
凤之兮原 <1577310596@qq.com>
2024-01-12 11:58:21 +0800

    feat: enhance gateway module

--- app/low-code-backend/src/module/gateway/gateway.gateway.ts
+++ app/low-code-backend/src/module/gateway/gateway.gateway.ts
@@ -9,6 +9,10 @@ import { Socket } from 'socket.io';
 import { Injectable } from '@nestjs/common';
 import { RedisService } from '../redis/redis.service';
 
+import { Project } from '../project/entities/project.entity';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -19,6 +23,8 @@ export class GatewayGateway {
   constructor(
     private readonly gatewayService: GatewayService,
     private readonly redisService: RedisService,
+    @InjectEntityManager()
+    private projectRepository: EntityManager,
   ) {}
 
   @SubscribeMessage('onJoin')
@@ -64,5 +70,48 @@ export class GatewayGateway {
     });
     // 更新当前画布信息至所有进入该画布的用户
     client.to(`canvas_${data.canvasId}`).emit('canvasUpdate', body);
+  }
+
+  @SubscribeMessage('onInvite')
+  async handleJoinProject(
+    @MessageBody() body: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const list: any[] =
+      JSON.parse(
+        await this.redisService.getRedis(`${body.uid}_list`, 'project'),
+      ) || [];
+
+    const project = await this.projectRepository.findOne(Project, {
+      where: {
+        project_id: body.project_id,
+      },
+    });
+    if (
+      list.some((i) => project.project_id !== i.project_id) ||
+      list.length === 0
+    ) {
+      list.push(project);
+      await this.redisService.setRedis(
+        `${body.uid}_list`,
+        'project',
+        JSON.stringify(list),
+      );
+    }
+    client.to(`${body.uid}_list`).emit('message', list);
+  }
+
+  @SubscribeMessage('onLine')
+  async handleMessage(
+    @MessageBody() body: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(`${body.uid}_list`);
+    client
+      .to(`${body.uid}_list`)
+      .emit(
+        'message',
+        await this.redisService.getRedis(`${body.uid}_list`, 'project'),
+      );
   }
 }
--- app/low-code-backend/src/module/gateway/gateway.module.ts
+++ app/low-code-backend/src/module/gateway/gateway.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
-import { GatewayService } from './gateway.service';
+import { CanvasService } from '../canvas/canvas.service';
 import { GatewayGateway } from './gateway.gateway';
+import { GatewayService } from './gateway.service';
+import { Module } from '@nestjs/common';
+import { ProjectService } from '../project/project.service';
 import { RedisModule } from '../redis/redis.module';
+import { RoleService } from '../role/role.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserCanvasProjectService } from 'src/joinTable/user_canvas_project/user_canvas_project.service';
+import { UserProjectRoleService } from 'src/joinTable/user_project_role/user_project_role.service';
+import { UserService } from '../user/user.service';
 
 @Module({
-  imports: [RedisModule],
-  providers: [GatewayGateway, GatewayService],
+  imports: [
+    RedisModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'syj011023',
+      database: 'low-code',
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  providers: [
+    GatewayGateway,
+    GatewayService,
+    ProjectService,
+    UserService,
+    RoleService,
+    UserProjectRoleService,
+    UserCanvasProjectService,
+    CanvasService,
+  ],
 })
 export class GatewayModule {}
