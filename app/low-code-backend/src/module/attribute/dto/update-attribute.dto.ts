f4a992f8e33141b5c319537b8e8dc6e6d9d3e6ca
凤之兮原 <1577310596@qq.com>
2024-02-04 11:35:22 +0800

    chore: remove attribute

--- app/low-code-backend/src/module/attribute/dto/update-attribute.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAttributeDto } from './create-attribute.dto';
-
-export class UpdateAttributeDto extends PartialType(CreateAttributeDto) {}
